ba6240ef790acf1ef2e5bd80647136fd1de47d7f
Felipe Santos <felipe.dss17@gmail.com>
2023-07-10 21:15:51 -0300

    busca por range de data e nome implementada

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("select new com.devsuperior.dsmeta.dto.SaleReportDTO (sale.id, sale.date, sale.amount, sale.seller.name)" +
+            " from Sale sale" +
+            " where sale.date between :dataInicial and :dataFinal" +
+            " and upper(sale.seller.name) like upper(concat('%',:nomeVendedor,'%'))" )
+    Page<SaleReportDTO> searchByDateRangeAndName(LocalDate dataInicial, LocalDate dataFinal, String nomeVendedor, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +23,14 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> findByDateRangeAndName(String dataInicial, String dataFinal, String nomeVendedor, Pageable pageable) {
+		LocalDate maxDate = dataFinal.isEmpty() ? LocalDate.now() :  LocalDate.parse(dataFinal);
+		LocalDate minDate = dataInicial.isEmpty() ? maxDate.minusYears(1l) : LocalDate.parse(dataInicial);
+
+		return repository.searchByDateRangeAndName(minDate, maxDate, nomeVendedor, pageable);
+
+
 	}
 }
